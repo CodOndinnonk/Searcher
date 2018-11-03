6fb76c756887491d45a2ae10f8ded487d1ec14dc
CodOndinnonk <ondinnonkme@gmail.com>
2018-11-03 13:11:55 +0200

    before selecting top words

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import com.sun.istack.internal.NotNull;
 import org.apache.log4j.Logger;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.*;
 
 
 public class Main {
@@ -19,7 +19,7 @@ public class Main {
     //    public static final File file = new File("D:\\Cloud\\GitHub\\Searcher\\extras\\test.bib");
     public static final String authorsSeparator = " and ";
     public static final String stringObjectDataSeparator = "\n";
-    public static final String authorToFind = "Jakub";
+    public static final String authorToFind = "J";
 
 
     public static void main(String[] args) {
@@ -45,7 +45,11 @@ public class Main {
         //overwrite list of read entities with splitted list
         readObjectList = separateAuthors(readObjectList);
 
-        findAuthor(readObjectList, authorToFind);
+
+        ArrayList<String> titles = findAuthor(readObjectList, authorToFind);
+
+        getTopWords(titles, 25);
+
     }
 
     /**
@@ -66,6 +70,13 @@ public class Main {
         return separatedAuthorsList;
     }
 
+    /**
+     * Perform search of authors posts from read object
+     *
+     * @param objects list of objects  to search in
+     * @param author  text to find
+     * @return list of titles associated with author
+     */
     private static ArrayList<String> findAuthor(ArrayList<ReadObject> objects, String author) {
         ArrayList<String> authorTitlesList = new ArrayList<String>();
 
@@ -77,6 +88,77 @@ public class Main {
             }
         }
         return authorTitlesList;
+    }
+
+
+    private static ArrayList<String> getTopWords(@NotNull ArrayList<String> titlesList, int topPercent) {
+        if (titlesList == null || titlesList.size() == 0) {
+            log.error("titlesList is empty.");
+            return null;
+        }
+        if (topPercent < 1 || topPercent > 100) {
+            log.error("Incorrect percent of top records. it has to be >= 1% and < 100%");
+            return null;
+        }
+
+        HashMap<String, Long> words = new HashMap<String, Long>();
+
+        //todo заменить на foreach
+        //get titles and separate by words
+        for (int i = 0; i < titlesList.size(); i++) {
+            if (titlesList.get(i) != null) {
+                String[] titleWords = titlesList.get(i).split(" ");
+                //perform calculation of words
+                for (int j = 0; j < titleWords.length; j++) {
+                    //if letters, not spec symbol
+//сделать регулярку на не буквы
+                    if (titleWords[j].toString().p)
+                    if (words.containsKey(titleWords[j])) {
+                        words.put(titleWords[j], words.get(titleWords[j]) + 1);
+                    } else {
+                        words.put(titleWords[j], Long.valueOf(1));
+                    }
+                }
+            }
+        }
+
+        int selectWordsAmt = words.size() / 100 * topPercent;
+
+
+        return null;
+    }
+
+
+    private static Map<String, Integer> sortByValue(Map<String, Integer> unsortedMap) {
+
+        //Convert Map to List of Map
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<Map.Entry<String, Integer>>(unsortedMap.entrySet());
+
+        //Sort list with Collections.sort(), provide a custom Comparator
+        //Try switch the o1 o2 position for a different order
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        //Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        /*
+        //classic iterator example
+        for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext(); ) {
+            Map.Entry<String, Integer> entry = it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }*/
+
+
+        return sortedMap;
     }
 
 
